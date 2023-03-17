60f59acd64307fe1efaf67d58edfacc7d22fdd9a
Soldy <4786022+Soldy@users.noreply.github.com>
2023-03-17 10:29:56 +0000

    test upgrade
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test.cpp
+++ test.cpp
@@ -10,7 +10,25 @@ TEST_CASE("simple test") {
     SUBCASE("get string") {
         CHECK(cppConfig::get("one") == "1");
     };
+    SUBCASE("get string with default") {
+        CHECK(cppConfig::get("one", 2) == "1");
+    };
+    SUBCASE("get string not exist") {
+        CHECK(cppConfig::get("not exist") == "");
+    };
+    SUBCASE("get string not exist with default") {
+        CHECK(cppConfig::get("not exist", "2") == "2");
+    };
     SUBCASE("get integer") {
         CHECK(cppConfig::getInt("one") == 1);
+    };
+    SUBCASE("get integer with default") {
+        CHECK(cppConfig::getInt("one", 2) == 1);
+    };
+    SUBCASE("get integer not exist") {
+        CHECK(cppConfig::getInt("not exist") == 0);
+    };
+    SUBCASE("get integer not exist with default") {
+        CHECK(cppConfig::getInt("not exist", 2) == 2);
     };
 };
